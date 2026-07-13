20a53436584e60e1d0d47bac6368619384740043
Braxton Joy <bdjoy2028@gmail.com>
2026-07-13 02:58:23 -0500

    update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var typed= new Typed(".text", {strings:["BLENDING IN" , "WASTING REVENUE" , "SABOTAGING MILLIONAIRE MOMENTUM"],
+var typed= new Typed(".text", {strings:["BLENDING IN" , "WASTING REVENUE" , "PREVENTING EXPANSION"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
