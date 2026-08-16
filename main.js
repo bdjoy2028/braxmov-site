16090b09495cd6232e43ce41a06d4e6d78d86f5b
Braxton Joy <bdjoy2028@gmail.com>
2026-08-15 21:52:22 -0500

    go go

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var typed= new Typed(".text", {strings:["Selling just Sqft" , "The Basic Listings" , "Validating Corporate Firms"],
+var typed= new Typed(".text", {strings:["Selling Just Sqft" , "The Basic Listings" , "Validating Corporate Firms"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
